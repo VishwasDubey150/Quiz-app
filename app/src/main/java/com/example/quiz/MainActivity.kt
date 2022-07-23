23aa630c2441b5aad48990d000318aa3e0fa5b90
Vishwas <VishwasDubey150@gmail.com>
2022-07-23 16:50:26 +0530

    First commit

---
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button_start:Button=findViewById(R.id.button_start)
+        var Entername:EditText=findViewById(R.id.Entername)
+        supportActionBar?.hide()
+        button_start.setOnClickListener {
+            if (Entername.text.isEmpty())
+            {
+                Toast.makeText(this,"Please Enter your name",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent=Intent(this,QuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quiz/splash.kt
@@ -0,0 +1,21 @@
+package com.example.quiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+    }
+}
